a926d0f9d56db3df3fc5b2408ad082d98e8c5b9e
tomtom304 <thomasrgparker@gmail.com>
2022-10-28 16:38:26 +0100

    Rewrite of Risk w/ Pop and Folders

---
+++ Risk with Pop.py
@@ -0,0 +1,77 @@
+from random import *
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+class agent:
+    pass
+
+n=10
+size=50
+fig=plt.figure()
+ax = plt.axes(xlim=(0, size), ylim=(0, size))
+grid = [[[-1,randint(1,10)] for i in range(size)] for j in range(size)]
+agents=[]
+for i in range(n):    
+    a=agent()
+    a.no=i
+    a.aggro=random()
+    a.x=randint(0,size-1)
+    a.y=randint(0,size-1)
+    a.squares=[a.x+a.y*size]
+    a.edgesquares=[a.x+a.y*size]
+    a.population=grid[a.x][a.y][1]
+    grid[a.x][a.y][0]=a.no
+    agents.append(a)
+
+
+bar=ax.bar([a.no for a in agents],[a.population for a in agents])
+change=1
+def neighbour(square,a,agents):
+    for i in (-1,0,1):
+        for j in (-1,0,1):
+            x=square%size+i
+            y=square//size+j
+            if abs(i+j)==1 and x not in (-1,size) and y not in (-1,size):
+                targets.append([grid[x][y][0],x,y])
+    return targets
+                
+while change!=0:
+    ax.clear()
+    for a in agents:
+        surrounded=True
+        while surrounded:
+            if len(a.edgesquares)!=0:
+                square=int(choice(a.edgesquares))
+                a.edgesquares.remove(square)
+            else:
+                surrounded=False
+            for i in (-1,0,1):
+                for j in (-1,0,1):
+                    x=square%size+i
+                    y=square//size+j
+                    if abs(i+j)==1 and x not in (-1,size) and y not in (-1,size):
+                        target=grid[x][y][0]
+                        if target==-1:
+                            grid[x][y][0]=a.no
+                            a.population+=grid[x][y][1]
+                            newsquare=x+y*size
+                            a.squares.append(newsquare)
+                            a.edgesquares.append(newsquare)
+                            surrounded=False
+                        elif agents[target].population<a.population:
+                            target=agents[target]
+                            grid[x][y][0]=a.no
+                            a.population+=grid[x][y][1]
+                            target.population-=grid[x][y][1]
+                            newsquare=x+y*size
+                            a.squares.append(x+y*size)
+                            a.edgesquares.append(newsquare)
+                            target.squares.remove(newsquare)
+                            try:
+                                target.edgesquares.remove(newsquare)
+                            except:
+                                pass
+            
+        #bar[a.no].set_height(a.population)
+        ax.plot([i%size for i in a.squares],[j//size for j in a.squares],"sC%d" %a.no)
+    plt.show(block=False)
+    plt.pause(0.1)
--- Risk/Risk.py
+++ Risk/Risk.py
@@ -4,8 +4,8 @@ from matplotlib.animation import FuncAnimation
 class agent:
     pass
 
-n=2
-size=20
+n=10
+size=50
 fig=plt.figure()
 ax = plt.axes(xlim=(0, size), ylim=(0, size))
 grid = [[-1 for i in range(size)] for j in range(size)]
@@ -18,11 +18,10 @@ for i in range(n):
     a.y=randint(0,size-1)
     a.squares=[a.x+a.y*size]
     a.edgesquares=[a.x+a.y*size]
+    a.size=1
     grid[a.x][a.y]=a.no
     agents.append(a)
 scatter=ax.scatter
-for a in agents:
-    ax.plot([i%size for i in a.squares],[j//size for j in a.squares],"sC%d" %a.no)
 plt.show(block=False)
 plt.pause(0.1)
 change=1
@@ -38,17 +37,19 @@ while change!=0:
             for target in [[grid[square//size+i][square%size+j],i,j] for i in (-1,0,1) for j in (-1,0,1) if abs(i+j)==1 and square//size+i not in (-1,size) and square%size+j not in (-1,size)]:
                 if target[0]==-1:
                     grid[square//size+target[1]][square%size+target[2]]=a.no
+                    a.size+=1
                     a.squares.append(square+target[2]+target[1]*size)
                     a.edgesquares.append(square+target[2]+target[1]*size)
                     surrounded=False
-                elif len(agents[target[0]].squares)<len(a.squares):
+                elif agents[target[0]].size<a.size:
                     grid[square//size+target[1]][square%size+target[2]]=a.no
                     a.squares.append(square+target[2]+target[1]*size)
                     a.edgesquares.append(square+target[2]+target[1]*size)
                     surrounded=False
-                    
+                    a.size+=1
                     try:
                         agents[target[0]].squares.remove(square+target[2]+target[1]*size)
+                        agents[target[0]].size-=1
                         agents[target[0]].edgesquares.remove(square+target[2]+target[1]*size)
                     except:
                         pass
