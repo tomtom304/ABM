8aec711391863cb856f50894f8e0b73477f0b4e9
tomtom304 <thomasrgparker@gmail.com>
2022-11-15 20:25:14 +0000

    Update Risk.py

--- Risk/Risk.py
+++ Risk/Risk.py
@@ -16,21 +16,10 @@ class baseagent:
             for j in (-1,0,1):
                 x=square%size+i
                 y=square//size+j
-                if abs(i+j)==1 and -1<x and x<size and y>-1 and y<size and grid[x][y]!=self.no:
+                if abs(i+j)==1 and -1<x and x<size and y>-1 and y<size:
                     targets.append([grid[x][y],x,y])
         return targets
-    def combat1(self,a,target,x,y):
-        return target.size<a.size
-    def combat2(self,a,target,x,y):
-        return a.size>random()*(a.size+target.size)
-    def combat3(self,a,target,x,y):
-        return False
-    def combat4(self,a,target,x,y):
-        return random()>0.5
-
     
-    def combat(self,a,target,x,y):
-        return self.combat2(a,target,x,y)
     
     def growth(self):
         surrounded=True
@@ -38,27 +27,27 @@ class baseagent:
             
             square=int(choice(self.edgesquares))
             self.edgesquares.remove(square)
-            targets=self.neighbour(square)
+            targets=[a for a in self.neighbour(square) if a[0]!=self.no]
             if len(targets)!=0 or len(self.edgesquares)==0:
                 surrounded=False
         
         for target in targets:
             targetno,x,y=target[0],target[1],target[2]
             targetagent=agents[targetno]
+            newsquare=x+y*size
             if targetno==-1:
                 grid[x][y]=self.no
                 self.size+=1
-                newsquare=x+y*size
+                
                 self.squares.append(newsquare)
                 self.edgesquares.append(newsquare)
-            elif self.combat(self,targetagent,x,y):
+            elif self.combat(self,targetagent,newsquare):
                 grid[x][y]=self.no
-                newsquare=x+y*size
                 try:
                     targetagent.edgesquares.remove(newsquare)
                 except:
                     pass
-                self.squares.append(x+y*size)
+                self.squares.append(newsquare)
                 self.edgesquares.append(newsquare)
                 targetagent.squares.remove(newsquare)
                 self.size+=1
@@ -67,7 +56,20 @@ class baseagent:
                 for newtarget in targetneighbour:
                     if newtarget[0]==targetagent.no:
                         targetagent.edgesquares.append(newtarget[1]+newtarget[2]*size)
-        
+    def combat1(self,a,targetagent,targetsquare):
+        return target.size<a.size
+    def combat2(self,a,targetagent,targetsquare):
+        return a.size>random()*(a.size+target.size)
+    def combat3(self,a,targetagent,targetsquare):
+        return False
+    def combat4(self,a,targetagent,targetsquare):
+        return random()>0.5
+    def combat5(self,a,targetagent,targetsquare):
+        combatants=self.neighbour(targetsquare)
+        return len([a for a in combatants if a[0]==self.no])>=len([a for a in combatants if a[0]==targetagent.no])
+    
+    def combat(self,a,targetagent,targetsquare):
+        return self.combat5(a,targetagent,targetsquare)    
 class agent8(baseagent):
     def __init__(self,no):
         super().__init__(no)
@@ -78,7 +80,7 @@ class agent8(baseagent):
             for j in (-1,0,1):
                 x=square%size+i
                 y=square//size+j
-                if abs(i)+abs(j)>0 and -1<x and x<size and y>-1 and y<size and grid[x][y]!=self.no:
+                if abs(i)+abs(j)>0 and -1<x and x<size and y>-1 and y<size:
                     targets.append([grid[x][y],x,y])
         return targets
 
@@ -101,32 +103,32 @@ class agent1(baseagent):
             grid[x][y]=self.no
             self.size+=1
             self.squares.append(target)
-            neighbours=self.neighbour(target)
+            neighbours=[a for a in self.neighbour(target) if a[0]!=self.no]
             self.edgesquares+=[i[1]+i[2]*size for i in neighbours]
                 
-        elif self.combat(a,targetagent,x,y):
+        elif self.combat(a,targetagent,target):
             grid[x][y]=a.no
             targetagent.edgesquares.append(target)
             self.squares.append(target)
             targetagent.squares.remove(target)
             self.size+=1
             targetagent.size-=1
-            targetneighbour=targetagent.neighbour(target)
+            targetneighbour=[a for a in targetagent.neighbour(target) if a[0]!=targetagent.no]
             self.edgesquares+=[i[1]+i[2]*size for i in targetneighbour]
 
 
-n=50
+n=5
 size=50
 fig,ax=plt.subplots(2)
 ax[0].set_ylim(0,size, auto=False)
 ax[0].set_xlim(0,size, auto=False)
 fig.set_size_inches(3.5, 6.75)
 fig.set_tight_layout(True)
-fig.set_dpi(120)
+fig.set_dpi(125)
 grid = [[-1 for i in range(size)] for j in range(size)]
 agents=[]
 for i in range(n):    
-    a=agent1(i)
+    a=agent8(i)
     agents.append(a)
 change=1
 
@@ -135,9 +137,9 @@ change=1
 bar=ax[1].bar(range(n),[a.size for a in agents],color=["C%d" %a.no for a in agents])
 while change!=0:
     ax[0].clear()
-    ax[0].set_ylim(0,size, auto=False)
-    ax[0].set_xlim(0,size, auto=False)
-    ax[1].set_ylim(0,2500, auto=True)
+    ax[0].set_ylim(-1,size, auto=False)
+    ax[0].set_xlim(-1,size, auto=False)
+    ax[1].set_ylim(0,size**2, auto=True)
     for a in agents:
         surrounded=True
         if len(a.edgesquares)!=0:
