c6635a55937839694daf2e4ff02039bc5e65e762
tomtom304 <thomasrgparker@gmail.com>
2022-11-23 21:24:24 +0000

    Update Risk.py

--- Risk/Risk.py
+++ Risk/Risk.py
@@ -42,6 +42,7 @@ class neighbourtypes:
     
 class growthtypes:
     def growthn(self,a):
+        new=[]
         surrounded=True
         while surrounded:
             square=int(choice(a.edgesquares))
@@ -58,6 +59,7 @@ class growthtypes:
                 a.size+=1
                 a.squares.append(newsquare)
                 a.edgesquares.append(newsquare)
+                new.append([x,y])
             elif a.combat(targetagent,newsquare):
                 grid[x][y]=a.no
                 try:
@@ -69,11 +71,11 @@ class growthtypes:
                 targetagent.squares.remove(newsquare)
                 a.size+=1
                 targetagent.size-=1
-                targetneighbour=targetagent.neighbour(newsquare)
-                for newtarget in targetneighbour:
-                    if newtarget[0]==targetagent.no:
-                        targetagent.edgesquares.append(newtarget[1]+newtarget[2]*size)                        
+                targetagent.edgesquares+=[i[1]+i[2]*size for i in targetagent.neighbour(newsquare) if i[0]==targetagent.no and i[1]+i[2]*size not in targetagent.edgesquares]
+                new.append([x,y])
+        return new
     def growth1(self,a):
+        new=[]
         target=choice(a.edgesquares)
         a.edgesquares.remove(target)
         x,y=target%size,target//size
@@ -84,18 +86,22 @@ class growthtypes:
             a.size+=1
             a.squares.append(target)
             neighbours=[newsquare for newsquare in a.neighbour(target) if newsquare[0]!=a.no]
-            a.edgesquares+=[i[1]+i[2]*size for i in neighbours]
+            a.edgesquares+=[i[1]+i[2]*size for i in neighbours if i[1]+i[2]*size not in a.edgesquares]
+            new.append([x,y])
+            
                 
         elif a.combat(targetagent,target):
             grid[x][y]=a.no
-            targetagent.edgesquares.append(target)
+            if target not in targetagent.edgesquares:
+                targetagent.edgesquares.append(target)
             a.squares.append(target)
             targetagent.squares.remove(target)
             a.size+=1
             targetagent.size-=1
             targetneighbour=[s for s in targetagent.neighbour(target) if s[0]!=targetagent.no]
-            a.edgesquares+=[i[1]+i[2]*size for i in targetneighbour]
-            
+            a.edgesquares+=[i[1]+i[2]*size for i in targetneighbour if i[1]+i[2]*size not in a.edgesquares]
+            new.append([x,y])
+        return new   
 class combattypes:           
     def combat1(self,a,targetagent,targetsquare): #functional but random
         return targetagent.size<a.size
@@ -128,7 +134,7 @@ class agent8(baseagent):
     def neighbour(self,square):
         return self.n.neighbour8(square)
     def growth(self):
-        self.g.growthn(self)
+        return self.g.growthn(self)
 class agent4(baseagent):
     def __init__(self,no):
         super().__init__(no)
@@ -139,7 +145,7 @@ class agent4(baseagent):
     def neighbour(self,square):
         return self.n.neighbour4(square)
     def growth(self):
-        self.g.growthn(self)
+        return self.g.growthn(self)
 class agent1(baseagent):
     def __init__(self,no):
         super().__init__(no)
@@ -151,17 +157,17 @@ class agent1(baseagent):
     def neighbour(self,square):
         return self.n.neighbour4(square)
     def growth(self):
-        self.g.growth1(self)
+        return self.g.growth1(self)
 
 
-class agent(agent8):
+class agent(agent4):
     def __init__(self,no):
         super().__init__(no)
         self.c=combattypes()
     def combat(self,targetagent,targetsquare):
         return self.c.combat6(self,targetagent,targetsquare)
     
-n=10
+n=3
 size=50
 fig,ax=plt.subplots(2)
 ax[0].set_ylim(0,size, auto=False)
@@ -174,19 +180,22 @@ agents=[]
 for i in range(n):    
     a=agent(i)
     agents.append(a)
+    ax[0].scatter([i%size for i in a.squares],[j//size for j in a.squares],marker="s",s=16)
 change=n
 
 bar=ax[1].bar(range(n),[a.size for a in agents],color=["C%d" %a.no for a in agents])
+ax[1].set_ylim(0,size**2, auto=True)
 while change!=0:
     change=n
     ax[0].clear()
-    ax[0].set_ylim(-1,size, auto=False)
-    ax[0].set_xlim(-1,size, auto=False)
-    ax[1].set_ylim(0,size**2, auto=True)
+    #ax[0].set_ylim(-1,size, auto=False)
+    #ax[0].set_xlim(-1,size, auto=False)
+    
     for a in agents:
-        surrounded=True
         if len(a.edgesquares)!=0:
-            a.growth()
+            new=a.growth()
+            #if new!=[]:
+                #ax[0].scatter([i[0] for i in new],[j[1] for j in new],"sC%d" %int(a.no))
         else:
             change-=1
         if a.size==size**2:
@@ -194,7 +203,7 @@ while change!=0:
                     
         ax[0].scatter([i%size for i in a.squares],[j//size for j in a.squares],marker="s",s=16)
         bar[a.no].set_height(a.size)
-        #ax.plot([i%size for i in a.edgesquares],[j//size for j in a.edgesquares],"sC%d" %int(a.no+5))
+        ax[0].scatter([i%size for i in a.edgesquares],[j//size for j in a.edgesquares],marker="s",s=16, color="C%d" %int(a.no+5))
     
     plt.show(block=False)
     plt.pause(0.001)
