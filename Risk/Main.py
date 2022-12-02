c908893766669b86fb5d0e5e4abad225615a5153
tomtom304 <thomasrgparker@gmail.com>
2022-12-02 19:50:46 +0000

    Update Main.py

--- Risk/Main.py
+++ Risk/Main.py
@@ -1,6 +1,6 @@
 from random import *
 import matplotlib.pyplot as plt
-
+import numpy as np
 class combattypes:           
     def combat1(self,a,targetagent,targetsquare): #functional but random
         return targetagent.size<a.size
@@ -17,7 +17,8 @@ class combattypes:
         combatants=a.neighbour(targetsquare)
         adv=int(a.size>targetagent.size)
         return len([s for s in combatants if s[0]==a.no])+adv>len([s for s in combatants if s[0]==targetagent.no])
-
+    #def combat7(self,a,targetagent,targetsquare):
+        
 
 
 class agent():
@@ -27,24 +28,39 @@ class agent():
         self.aggro=random()
         self.x=randint(0,xsize-1)
         self.y=randint(0,ysize-1)
-        while grid[self.x][self.y][0]!=-1 or grid[self.x][self.y][1]==1:
+        while grid[self.x][self.y]["owner"]!=-1 or grid[self.x][self.y]["terrain"]==1:
             self.x=randint(0,xsize-1)
             self.y=randint(0,ysize-1)
         self.squares=[self.x+self.y*xsize]
         self.size=1
-        grid[self.x][self.y][0]=self.no
+        grid[self.x][self.y]["owner"]=self.no
+        grid[self.x][self.y]["pop"]=1
         self.c=combattypes()
         self.edgesquares=[self.x+self.y*xsize]
     def combat(self,targetagent,targetsquare,terrain):
         return self.c.combat2(self,targetagent,targetsquare)
+    def gainsquare(self,x,y,square):
+        grid[x][y]["owner"]=self.no
+        grid[x][y]["pop"]=1
+        self.size+=1
+        self.squares.append(square)
+        self.edgesquares.append(square)
+    def losesquare(self,a,x,y,square):
+        self.edgesquares.remove(square)
+        self.squares.remove(square)
+        self.size-=1
+        for i in a.neighbour(square):
+            if i[0] not in (a.no,-1):
+                if i[1]+i[2]*xsize not in agents[i[0]].edgesquares:
+                    agents[i[0]].edgesquares.append(i[1]+i[2]*xsize)
     def neighbour(self,square):
         targets=[]
         for i in (-1,0,1):
             for j in (-1,0,1):
                 x=square%xsize+i
                 y=square//xsize+j
-                if abs(i+j)==1 and -1<x and x<xsize and y>-1 and y<ysize and grid[x][y][1]!=1:
-                    targets.append([grid[x][y][0],x,y,grid[x][y][1]])
+                if abs(i+j)==1 and -1<x and x<xsize and y>-1 and y<ysize and grid[x][y]["terrain"]!=1:
+                    targets.append([grid[x][y]["owner"],x,y,grid[x][y]["terrain"]])
         return targets
     def growth(self):
         new=0
@@ -64,26 +80,11 @@ class agent():
             newsquare=x+y*xsize
             if terrain!=1:
                 if targetno==-1:
-                    grid[x][y][0]=self.no
-                    self.size+=1
-                    self.squares.append(newsquare)
-                    self.edgesquares.append(newsquare)
+                    self.gainsquare(x,y,newsquare)
                     new+=1
                 elif self.combat(targetagent,newsquare,terrain):
-                    grid[x][y][0]=self.no
-                    try:
-                        targetagent.edgesquares.remove(newsquare)
-                    except:
-                        print(newsquare%xsize,newsquare//xsize)
-                    self.squares.append(newsquare)
-                    self.edgesquares.append(newsquare)
-                    targetagent.squares.remove(newsquare)
-                    self.size+=1
-                    targetagent.size-=1
-                    for i in self.neighbour(newsquare):
-                        if i[0] not in (self.no,-1):
-                            if i[1]+i[2]*xsize not in agents[i[0]].edgesquares:
-                                agents[i[0]].edgesquares.append(i[1]+i[2]*xsize)
+                    self.gainsquare(x,y,newsquare)
+                    targetagent.losesquare(self,x,y,newsquare)
                     new+=1
         return new==0
     
@@ -96,7 +97,7 @@ ax[0].set_xlim(0,xsize, auto=False)
 fig.set_size_inches(5.25, 6.75)
 fig.set_tight_layout(True)
 fig.set_dpi(125)
-grid = [[[-1,0] for i in range(ysize)] for j in range(xsize)]
+grid = np.array([[{"owner":-1,"terrain":0,"pop":0} for i in range(ysize)] for j in range(xsize)])
 border=[]
 for i in range(ysize-1):
     border.append([25,i])
@@ -104,7 +105,7 @@ for i in range(ysize-1):
 for i in range(27):
     border.append([i+27,25])
 for i in border:
-    grid[i[0]][i[1]][1]=1
+    grid[i[0]][i[1]]["terrain"]=1
 blocked=len(border)
 ax[0].scatter([i[0] for i in border],[i[1] for i in border],color="black",marker="s",s=16)
 agents=[]
@@ -116,7 +117,7 @@ change=n
 end=0
 bar=ax[1].bar(range(n),[a.size for a in agents],color=["C%d" %a.no for a in agents])
 ax[1].set_ylim(0,xsize*ysize, auto=True)
-while end<3:
+while end<5:
     change=n
     ax[0].clear()
     ax[0].set_ylim(-1,ysize, auto=False)
